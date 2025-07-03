14b01dd37459ea99906b6606237ba32b080781a0
Ash Kuhlmann <14260109+AshKuhlmann@users.noreply.github.com>
2025-07-02 23:58:42 -0700

    Add human_readable_size util and tests

---
+++ tests/test_cli_utils.py
@@ -0,0 +1,8 @@
+import pytest
+from sorter import cli_utils
+
+
+def test_human_readable_size():
+    assert cli_utils.human_readable_size(1024) == "1.0 KB"
+    assert cli_utils.human_readable_size(0) == "0.0 B"
+    assert cli_utils.human_readable_size(1024 * 1024 * 5) == "5.0 MB"
